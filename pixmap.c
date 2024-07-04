77d0a7beb25805412171ee45e994f0509b8592d9
Johan Malm <jgm323@gmail.com>
2024-07-04 21:01:31 +0100

    Render svg icons too

--- pixmap.c
+++ pixmap.c
@@ -4,6 +4,7 @@
 #include <cairo.h>
 #include <ctype.h>
 #include <glib.h>
+#include <librsvg/rsvg.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <pango/pangocairo.h>
@@ -19,6 +20,82 @@
 #include <unistd.h>
 #include "menu.h"
 #include "trappist.h"
+
+static void
+add_svg(cairo_t *cairo, const char *filename, int icon_size)
+{
+	GError *err = NULL;
+	RsvgRectangle viewport = { .width = icon_size, .height = icon_size };
+	RsvgHandle *svg = rsvg_handle_new_from_file(filename, &err);
+	if (err) {
+		LOG(LOG_DEBUG, "error reading svg %s-%s", filename, err->message);
+		g_error_free(err);
+		return;
+	}
+
+	cairo_surface_t *image = cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
+		icon_size, icon_size);
+	cairo_t *cr = cairo_create(image);
+
+	rsvg_handle_render_document(svg, cr, &viewport, &err);
+	if (err) {
+		LOG(LOG_ERROR, "error rendering svg %s-%s\n", filename, err->message);
+		g_error_free(err);
+		goto error;
+	}
+
+	if (cairo_surface_status(image)) {
+		LOG(LOG_ERROR, "error reading svg button '%s'", filename);
+		goto error;
+	}
+	cairo_surface_flush(image);
+
+	cairo_translate(cairo, MENU_ITEM_PADDING_X, MENU_ITEM_PADDING_Y);
+
+	double w = cairo_image_surface_get_width(image);
+	double h = cairo_image_surface_get_height(image);
+	double x = (icon_size - w) / 2.0;
+	double y = (icon_size - h) / 2.0;
+	cairo_set_source_surface(cairo, image, x, y);
+	cairo_paint_with_alpha(cairo, 1.0);
+
+error:
+	cairo_destroy(cr);
+	cairo_surface_destroy(image);
+	g_object_unref(svg);
+}
+
+static void
+add_png(cairo_t *cairo, const char *filename, int icon_size)
+{
+	cairo_surface_t *image = cairo_image_surface_create_from_png(filename);
+	if (cairo_surface_status(image)) {
+		cairo_surface_destroy(image);
+		LOG(LOG_ERROR, "bad png icon (%s)", filename);
+		return;
+	}
+
+	cairo_translate(cairo, MENU_ITEM_PADDING_X, MENU_ITEM_PADDING_Y);
+
+	/* TODO: use cairo_image_surface_scale() */
+	double w, h, max;
+	w = cairo_image_surface_get_width(image);
+	h = cairo_image_surface_get_height(image);
+	max = h > w ? h : w;
+	if (max != icon_size) {
+		cairo_scale(cairo, icon_size / max, icon_size / max);
+	}
+	cairo_set_source_surface(cairo, image, 0, 0);
+	cairo_paint_with_alpha(cairo, 1.0);
+	cairo_surface_destroy(image);
+}
+
+bool
+ends_with(const char *string, const char *ending)
+{
+	size_t pos = strlen(string) - strlen(ending);
+	return strcmp(string+pos, ending) == 0;
+}
 
 static void
 render_menu_entry(cairo_surface_t **pixmap, struct menuitem *item, uint32_t color)
@@ -46,30 +123,13 @@ render_menu_entry(cairo_surface_t **pixmap, struct menuitem *item, uint32_t colo
 	}
 
 	if (item->icon && *item->icon) {
-		cairo_surface_t *image = NULL;
-		image = cairo_image_surface_create_from_png(item->icon);
-		if (cairo_surface_status(image)) {
-			cairo_surface_destroy(image);
-			goto out;
+		if (ends_with(item->icon, ".png")) {
+			add_png(cairo, item->icon, icon_size);
+		} else if (ends_with(item->icon, ".svg")) {
+			add_svg(cairo, item->icon, icon_size);
 		}
-
-		cairo_translate(cairo, MENU_ITEM_PADDING_X, MENU_ITEM_PADDING_Y);
-
-		/* TODO: use cairo_image_surface_scale() */
-		double w, h, max;
-		w = cairo_image_surface_get_width(image);
-		h = cairo_image_surface_get_height(image);
-		max = h > w ? h : w;
-		if (max != icon_size) {
-			cairo_scale(cairo, icon_size / max, icon_size / max);
-		}
-
-		cairo_set_source_surface(cairo, image, 0, 0);
-		cairo_paint_with_alpha(cairo, 1.0);
-		cairo_surface_destroy(image);
 	}
 
-out:
 	cairo_destroy(cairo);
 }
 
